1bed6a369fe8cb25608111ca33830c1e0bd428bb
JuJu <45171747+JUJUmoe@users.noreply.github.com>
2023-05-07 22:01:21 +0800

    Added the function of obtaining avatar

--- app.js
+++ app.js
@@ -1,35 +1,59 @@
-// Import the discord.js library
+// Import discord.js library and load dotenv module
 const Discord = require('discord.js');
- require('dotenv').config();
-
-// Create a new client instance
+// Initialize the client and get the bot token from environment variables
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
-// Get the bot token from the environment variable
-const token = ''; //BOT token
+const token =' '; //bot token
 
+// Load the required modules
+const axios = require('axios');
 
 // When the client is ready, log a message
-client.once('ready', () => {
-  console.log('Ready!');
+client.on('ready', () => {
+console.log('Bot is ready !');
 });
 
 // When the client receives an interaction, handle it
 client.on('interactionCreate', async interaction => {
-  // If the interaction is not a command, ignore it
-  if (!interaction.isCommand()) return;
-  // Get the command name and options
-  const { commandName, options } = interaction;
-  // If the command is ping, reply with latency and hosting provider
-  if (commandName === 'ping') {
+// If the interaction is not a command, ignore it.
+if (!interaction.isCommand()) return;
+
+// Get the command name and options.
+const { commandName, options } = interaction;
+
+// Check the command name
+if (commandName === 'ping') {
     // Get the current gateway latency
     const latency = client.ws.ping;
     // Get the hosting provider name
-    const provider = ''; //托管商
+    const provider = ' '; //idc服务商
+    // Get the IP address of the server
+    const ip = await axios.get('https://api.ipify.org/?format=json');
+    // Get the location of the IP address
+    const location = await axios.get(`https://freegeoip.app/json/${ip.data.ip}`);
+
     // Build the reply message
-    const message = `Pong! Latency: ${latency} ms. Hosting provider: ${provider}.`;
+    const message = `Pong! Latency: ${latency} ms. Hosting provider: ${provider}. \nServer IP: ${ip.data.ip}, Location: ${location.data.city}, ${location.data.region_name}, ${location.data.country_name}`;
     // Reply to the interaction
     await interaction.reply(message);
-  }
+}
+else if (commandName === 'avatar') {
+    // Get the mentioned user from the options
+    const user = options.getUser('user');
+
+    // If no user is provided, use the interaction user.
+    if (!user) {
+        user = interaction.user;
+    }
+
+    // Get the user's avatar with a 1024x1024 size
+    const avatar = user.displayAvatarURL({ format: 'png', size: 1024 });
+
+    // Build the reply message
+    const message = `Here's the avatar of ${user.username}: ${avatar}`;
+
+    // Reply to the interaction
+    await interaction.reply(message);
+}
 });
 
 // Login to Discord with the bot token
